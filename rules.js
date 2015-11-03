36de75ab83b306e3374c01b9ee2b97428fbb4b0d
Vikash Bajaj <contactvikashbajaj@gmail.com>
2015-11-03 06:43:25 -0700

    restructured code and added more rules

--- rules.js
+++
@@ -1,6 +0,0 @@
-rules = {
-          'mail.google.com' : 'mail.google.com/mail/u/#search/{{search_terms}}',
-          'youtube.com': 'youtube.com/results?search_query={{search_terms}}',
-          'www.youtube.com': 'youtube.com/results?search_query={{search_terms}}',
-          'news.ycombinator.com' : 'hn.algolia.com/?query={{search_terms}}'
-}
---
+++ src/rules.js
@@ -0,0 +1,11 @@
+rules = {
+          'mail.google.com' : 'mail.google.com/mail/u/#search/{{search_terms}}',
+          'www.youtube.com': 'youtube.com/results?search_query={{search_terms}}',
+          'news.ycombinator.com' : 'hn.algolia.com/?query={{search_terms}}',
+          'google.com' : 'google.com/search?q={{search_terms}}',
+          'www.amazon.com' : 'amazon.com/exec/obidos/external-search/?field-keywords={{search_terms}}',
+          'en.wikipedia.org' : 'en.wikipedia.org/wiki/Special:Search?search={{search_terms}}',
+          'yahoo.com' : 'search.yahoo.com/search?p={{search_terms}}',
+          'search.yahoo.com' : 'search.yahoo.com/search?p={{search_terms}}',
+          'twitter.com' : 'twitter.com/search?q={{search_terms}}'
+}
